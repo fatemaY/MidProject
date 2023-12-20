3249e06f118584e7ded2400774359286e2fa6222
fatemaY <fatemayasen2@gmail.com>
2023-12-20 20:10:49 +0200

    changed <o> to <p>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './app.css';
+import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import About from './components/about/About';
